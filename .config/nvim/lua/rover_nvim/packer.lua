df5f7caee8922a85a201ac74c224df2801c893c0
Thee-RoVeR <harshadeshik1@gmail.com>
2023-08-13 04:58:07 +0530

    added & modified polybar and made some changes

--- .config/nvim/lua/rover_nvim/packer.lua
+++ .config/nvim/lua/rover_nvim/packer.lua
@@ -4,54 +4,60 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-	-- Packer can manage itself
-	use 'wbthomason/packer.nvim'
+        -- Packer can manage itself
+        use 'wbthomason/packer.nvim'
 
-	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.2',
-		-- or                            , branch = '0.1.x',
-		requires = { {'nvim-lua/plenary.nvim'} }
-	}
-	use({
-		'morhetz/gruvbox',
-		as = 'gruvbox',
-		config = function()
-			vim.cmd('colorscheme gruvbox')
-		end
-	})
-	use('nvim-treesitter/nvim-treesitter',  {run = ':TSUpdate'})
-	use('nvim-treesitter/playground')
-	use('theprimeagen/harpoon')
-	use('mbbill/undotree')
-	use('tpope/vim-fugitive')
-	use {
-		'VonHeikemen/lsp-zero.nvim',
-		branch = 'v1.x',
-		requires = {
-			-- LSP Support
-			{'neovim/nvim-lspconfig'},
-			{'williamboman/mason.nvim'},
-			{'williamboman/mason-lspconfig.nvim'},
+        use {
+                'nvim-telescope/telescope.nvim', tag = '0.1.2',
+                -- or                            , branch = '0.1.x',
+                requires = { {'nvim-lua/plenary.nvim'} }
+        }
+        use({
+                'morhetz/gruvbox',
+                as = 'gruvbox',
+                config = function()
+                        vim.cmd('colorscheme gruvbox')
+                end
+        })
+        use('nvim-treesitter/nvim-treesitter',  {run = ':TSUpdate'})
+        use('nvim-treesitter/playground')
+        use('theprimeagen/harpoon')
+        use('mbbill/undotree')
+        use('tpope/vim-fugitive')
+        use {
+                'VonHeikemen/lsp-zero.nvim',
+                branch = 'v1.x',
+                requires = {
+                        -- LSP Support
+                        {'neovim/nvim-lspconfig'},
+                        {'williamboman/mason.nvim'},
+                        {'williamboman/mason-lspconfig.nvim'},
 
-			-- Autocompletion
-			{'hrsh7th/nvim-cmp'},
-			{'hrsh7th/cmp-buffer'},
-			{'hrsh7th/cmp-path'},
-			{'saadparwaiz1/cmp_luasnip'},
-			{'hrsh7th/cmp-nvim-lsp'},
-			{'hrsh7th/cmp-nvim-lua'},
+                        -- Autocompletion
+                        {'hrsh7th/nvim-cmp'},
+                        {'hrsh7th/cmp-buffer'},
+                        {'hrsh7th/cmp-path'},
+                        {'saadparwaiz1/cmp_luasnip'},
+                        {'hrsh7th/cmp-nvim-lsp'},
+                        {'hrsh7th/cmp-nvim-lua'},
 
-			-- Snippets
-			{'L3MON4D3/LuaSnip'},
-			{'rafamadriz/friendly-snippets'},
-		}
-	}
-	use {
-		'neoclide/coc.nvim',
-		branch = 'release',
-		config = function()
-			vim.g.coc_global_extensions = { 'coc-clangd' }
-		end
-	}
+                        -- Snippets
+                        {'L3MON4D3/LuaSnip'},
+                        {'rafamadriz/friendly-snippets'},
+                }
+        }
+        use {
+                'neoclide/coc.nvim',
+                branch = 'release',
+                config = function()
+                        vim.g.coc_global_extensions = { 'coc-clangd' }
+                end
+        }
 
+        use {
+        'norcalli/nvim-colorizer.lua',
+        config = function()
+            require('colorizer').setup()
+        end
+    }
 end)
--- .config/nvim/plugin/packer_compiled.lua
+++ .config/nvim/plugin/packer_compiled.lua
@@ -146,6 +146,12 @@ _G.packer_plugins = {
     path = "/home/rover/.local/share/nvim/site/pack/packer/start/nvim-cmp",
     url = "https://github.com/hrsh7th/nvim-cmp"
   },
+  ["nvim-colorizer.lua"] = {
+    config = { "\27LJ\2\n7\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\14colorizer\frequire\0" },
+    loaded = true,
+    path = "/home/rover/.local/share/nvim/site/pack/packer/start/nvim-colorizer.lua",
+    url = "https://github.com/norcalli/nvim-colorizer.lua"
+  },
   ["nvim-lspconfig"] = {
     loaded = true,
     path = "/home/rover/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
@@ -197,6 +203,10 @@ time([[Config for gruvbox]], false)
 time([[Config for coc.nvim]], true)
 try_loadstring("\27LJ\2\nF\0\0\2\0\4\0\0056\0\0\0009\0\1\0005\1\3\0=\1\2\0K\0\1\0\1\2\0\0\15coc-clangd\26coc_global_extensions\6g\bvim\0", "config", "coc.nvim")
 time([[Config for coc.nvim]], false)
+-- Config for: nvim-colorizer.lua
+time([[Config for nvim-colorizer.lua]], true)
+try_loadstring("\27LJ\2\n7\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\14colorizer\frequire\0", "config", "nvim-colorizer.lua")
+time([[Config for nvim-colorizer.lua]], false)
 
 _G._packer.inside_compile = false
 if _G._packer.needs_bufread == true then
