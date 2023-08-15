0029c2134c0bbdcd7389fd08d9b669e5e24c7da1
Thee-RoVeR <harshadeshik1@gmail.com>
2023-08-15 21:21:05 +0530

    made very small changes to picom and bashrc

--- .bashrc
+++ .bashrc
@@ -10,9 +10,8 @@ alias grep='grep --color=auto'
 
 export PS1='\n\[\e[1;34m\]\h\[\e[0m\] \[\e[1;38m\]as \[\e[1;33m\]\u\[\e[0m\]  \[\e[1;32m\]✅:\[\e[1;32m\]\w\[\e[1;0m\]\n|➤  '
 
-neofetch
-
 #custom alias
 alias 'v'='nvim'
 alias 'wifi-off'='sudo nmcli networking off'
 alias 'wifi-on'='sudo nmcli networking on'
+alias 'htop'='bashtop'
--- rice.sh
+++ rice.sh
@@ -1,16 +1,16 @@
-printf "Running ricing script \n You might need to provide password and cofirmation.\n Keep an eye on the terminal\n"
+printf " Running ricing script \n You might need to provide password and cofirmation.\n Keep an eye on the terminal\n"
 
 ./scripts/base.sh
 
-printf "\n all required downloades are done\n"
-printf "\n----------------------------------------------\n"
+printf "\n all required downloades are done \n"
+printf "\n---------------------------------------------- \n"
 
 printf "\n Setting-up Lightdm hang tight \n"
 ./scripts/lightdm.sh
 
-printf "\n Lightdm setup complete\n"
-printf "\n----------------------------------------------\n"
-printf "\nsetting up i3wm rice, hang om a sec ^,,^ \n"
+printf "\n Lightdm setup complete \n"
+printf "\n---------------------------------------------- \n"
+printf "\n setting up i3wm rice, hang om a sec ^,,^ \n"
 
 ./scripts/i3wmconfig.sh
 printf "\n i3WM Ricing is done \n"
